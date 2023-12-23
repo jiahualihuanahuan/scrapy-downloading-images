877a2143b8fa2d401ddb3861d64046de4b56ad05
Jiahua Li <59403109+jiahualihuanahuan@users.noreply.github.com>
2023-12-22 20:30:15 -0500

    Update items.py

--- items.py
+++ items.py
@@ -6,7 +6,7 @@
 import scrapy
 
 
-class PianoItem(scrapy.Item):
+class ImageItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     image_urls = scrapy.Field()
