b9123cd71f0df94e79b4eb50a59109f4df092c44
Jiahua Li <59403109+jiahualihuanahuan@users.noreply.github.com>
2023-12-29 20:46:09 -0500

    Update imgdl.py

--- imgdl.py
+++ imgdl.py
@@ -4,12 +4,19 @@ from scrapy.spiders import CrawlSpider, Rule
 from image.items import ImageItem
 
 class ImgdlSpider(CrawlSpider):
-    name = "imgdl"
-    allowed_domains = ["meitu131.com","ertuba.com"]
-    start_urls = ["https://www.meitu131.com/meinv"]
+    name = "imgdl" # this is the name of the spider/crawler
+    allowed_domains = ["meitu131.com","ertuba.com"] # this is the allowed domains this crawler can access, you can remove this line if you want to explore all linked webpages
+    start_urls = ["https://www.meitu131.com/meinv"] # the starting url
 
     rules = (
-        Rule(LinkExtractor(allow=r"meinv/[0-9]{1,8}"), callback="parse_item", follow=True),
+        Rule(LinkExtractor(allow=r"meinv/[0-9]{1,8}", deny=r"shouji"), callback="parse_item", follow=True), 
+        # in the rules section, the crawler will extract all links that follow the rule but only once.
+        # please make sure the rule is built in a way which can fullfill you requirements
+        # r"" is regular expression
+        # allow is to only go to the url (extract links) which follow the regular expression
+        # deny is not go to the url which follow the regular expression
+        # callback is that if the link satisfy condition (allow and deny) then go to the function and perform specific scraping function
+        # follow means if you wish to extract all links from this url
             )
 
     def parse_item(self, response):
